89fb0f8bf4b1697a0192861061b73e64695f3d51
MSA <crystalbluetech@gmail.com>
2024-05-07 16:20:24 +0100

    display blog list dynamically by maping the useState default state

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,49 @@
 import { useState } from "react";
 const Home = () => {
-    const [name, setName] = useState('crystal'); // name is the state variable, setName is the function to update the state variable, 'crystal' is the initial value of the state variable
-    const [age, setAge] = useState(25); // age is the state variable, setAge is the function to update the state variable, 25 is the initial value of the state variable
-
-
-
-
-    const handleClick = () => {
-      
-       setName('Crystal Blue'); // setName is the function to update the state variable
-         setAge(30); // setAge is the function to update the state variable
-    }
-
+    const [blogs, setBlogs] = useState([
+        {
+          "id": 1,
+          "title": "The Future of AI in Healthcare",
+          "body": "Artificial Intelligence (AI) is revolutionizing the healthcare industry...",
+          "author": "Muhammad Salihu"
+        },
+        {
+          "id": 2,
+          "title": "5G Technology: Transforming Connectivity",
+          "body": "The advent of 5G technology promises faster speeds, lower latency, and...",
+          "author": "Hauwa Muhammad"
+        },
+        {
+          "id": 3,
+          "title": "Blockchain: Beyond Cryptocurrency",
+          "body": "While blockchain technology gained fame through cryptocurrencies like Bitcoin...",
+          "author": "Zainab Muhammad"
+        },
+        {
+          "id": 4,
+          "title": "The Rise of Virtual Reality in Gaming",
+          "body": "Virtual Reality (VR) technology is reshaping the gaming landscape, offering immersive experiences...",
+          "author": "Sophia Chen"
+        },
+        {
+          "id": 5,
+          "title": "Cybersecurity Trends in 2024",
+          "body": "With cyber threats evolving rapidly, staying ahead of the curve is crucial...",
+          "author": "Michael Adams"
+        }
+      ]
+      ); 
  
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age  } years old </p>
-            {/* handleClick() or handleClick('crystal') will invoke the fuction automatically*/}
-            <button onClick={handleClick}>Click me</button>
-           
+         {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{ blog.title }</h2>
+                <p>Written by { blog.author }</p>
+            </div>
+         ))}
+            
         </div>
     );
 }
