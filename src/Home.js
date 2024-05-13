9203b61e0039ecb30a7112a7e3e5156ccca75077
MSA <crystalbluetech@gmail.com>
2024-05-13 15:11:43 +0100

    fetch data from json server

--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,7 @@
 import { useState, useEffect } from "react";
 import BlogList from "./blogList";
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {
-          "id": 1,
-          "title": "The Future of AI in Healthcare",
-          "body": "Artificial Intelligence (AI) is revolutionizing the healthcare industry...",
-          "author": "Muhammad Salihu"
-        },
-        {
-          "id": 2,
-          "title": "5G Technology: Transforming Connectivity",
-          "body": "The advent of 5G technology promises faster speeds, lower latency, and...",
-          "author": "Hauwa Muhammad"
-        },
-        {
-          "id": 3,
-          "title": "Blockchain: Beyond Cryptocurrency",
-          "body": "While blockchain technology gained fame through cryptocurrencies like Bitcoin...",
-          "author": "Zainab Muhammad"
-        },
-        {
-          "id": 4,
-          "title": "The Rise of Virtual Reality in Gaming",
-          "body": "Virtual Reality (VR) technology is reshaping the gaming landscape, offering immersive experiences...",
-          "author": "Muhammad Salihu"
-        },
-        {
-          "id": 5,
-          "title": "Cybersecurity Trends in 2024",
-          "body": "With cyber threats evolving rapidly, staying ahead of the curve is crucial...",
-          "author": "Muhammad Salihu"
-        }
-      ]
-      ); 
+    const [blogs, setBlogs] = useState(null); 
 
       const [name, setName] = useState('crystal')
 
@@ -44,16 +12,21 @@ const Home = () => {
 
       // UseEffect: runs a function on every render of the component
       useEffect(() => {
-        console.log('useeffect ran')
-        console.log(name);
-      }, [name]);
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json();
+        })
+        .then((data) => {
+        setBlogs(data)
+        })
+      }, []);
  
 
     return (
         <div className="home">
-        <BlogList blogs={blogs} title="All Tech Blogs" handleDelete={handleDelete}/> 
-        <BlogList blogs={ blogs.filter((blog) => blog.author === 'Muhammad Salihu')}
-         title="Muhammad's Blogs" handleDelete={handleDelete}/>
+        {blogs && <BlogList blogs={blogs} title="All Tech Blogs" handleDelete={handleDelete}/> }
+        {blogs && <BlogList blogs={ blogs.filter((blog) => blog.author === 'Muhammad Salihu')} 
+         title="Muhammad's Blogs" handleDelete={handleDelete}/> }
          <button onClick={() => setName('Crystal Blue') }>change name</button>
          <p>{ name }</p>
         </div>
