ca533a8f332cc041f24cb0e0cbbcf7874cb138df
MSA <crystalbluetech@gmail.com>
2024-05-19 22:36:57 +0100

    add to avoid sending request to server on every click

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,15 @@
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>Crystal Blue Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
                     color: "white",
                     backgroundColor: "#3a9bdc",
                     borderRadius: '8px' 
-                 }}>New Blog</a>
+                 }}>New Blog</Link>
             </div>
         </nav>
     );
