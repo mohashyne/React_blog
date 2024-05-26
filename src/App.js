10b4b14b36e5cf49e62c8ba47f37bcc489165227
MSA <crystalbluetech@gmail.com>
2024-05-26 23:13:23 +0100

    add page not found error message component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
 
@@ -25,9 +26,9 @@ function App() {
           <Route path="/blogs/:id">
             <BlogDetails />
           </Route>
-          {/* <Route path="*">
+          <Route path="*">
             <NotFound />
-          </Route> */}
+          </Route>
         </Switch>
         </div>
             </div>
