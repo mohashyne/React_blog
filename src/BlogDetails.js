7ae02217a17dce6332a47ad610cd8469948e829f
MSA <crystalbluetech@gmail.com>
2024-05-26 22:59:49 +0100

    delete button and func to handle blog deletion

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,21 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom"; 
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const pageHistory = useHistory();
+
+    const handleClick = () => {
+      fetch('http://localhost:8000/blogs/' + blog.id, {
+        method:"DELETE"
+      })
+      .then(() => {
+       pageHistory.push('/')
+      })
+
+    }
     
 
     return ( 
@@ -15,6 +27,7 @@ const BlogDetails = () => {
             <h2>{ blog.title }</h2>
             <p>Written by { blog.author }</p>
             <div>{ blog.body }</div>
+            <button onClick={handleClick} >delete blog</button>
         </article>
     )}
     </div>
